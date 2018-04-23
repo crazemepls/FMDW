ad1e7c684a07b28f2238e37ac0c4b51f6097db34
crazemepls <38428584+crazemepls@users.noreply.github.com>
2018-04-23 12:29:48 +0700

    html polos login

--- FMDW/login.php
+++ FMDW/login.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Login to FMDW</title>
 </head>
 <body>
-
+	<div>
+		<img src="logo">
+		<p>LOGIN SYSTEM</p>
+		<div>
+			<p>LOGIN</p>
+			<img src="logo wong" class="logo insert">
+			<form>
+				<input type="text" name="email" placeholder="Email">
+				<img src="logo gembok" class="logo insert">
+				<input type="password" name="password" placeholder="Password">	
+				<input type="checkbox" name="remember" value="remember_yes"> remember me
+				<br/>
+				<br/>
+				<input type="submit" name="login" value="Login">
+			</form>
+			<a href="fogot password">Fogot Passwod?</a>
+		</div>
+	</div>
+	
+	<footer id="footer">
+	</footer>
 </body>
 </html>
